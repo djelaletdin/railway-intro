8fd95113650d3dbae7da92e7f3ed3a955bccae6a
djelaletdin <jelaletdinov@gmail.com>
2025-02-14 11:01:30 +0500

    Created AttributesCardComponent.vue

--- resources/js/Components/AttributesCardComponent.vue
+++ resources/js/Components/AttributesCardComponent.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
+    <div class="flex flex-wrap justify-center gap-8">
         <div v-for="attribute in attributes"
              :key="attribute.id"
-             class="bg-white rounded-lg shadow p-6 hover:shadow-lg transition-shadow">
+             class="bg-white rounded-lg shadow px-10 py-6 ring-gray-200 ring-1 hover:shadow-lg transition-shadow">
             <div class="text-gray-500 text-sm mb-1">{{ attribute.name }}</div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
+            </svg>
 
             <!-- Number type attributes -->
             <div v-if="attribute.type === 'number'"
@@ -25,7 +28,6 @@
           {{ value }}
         </span>
             </div>
-
             <div class="text-gray-400 text-xs mt-2">{{ attribute.description }}</div>
         </div>
     </div>
